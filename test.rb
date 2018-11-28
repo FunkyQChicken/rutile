a6f92c7bc00da881d898795926498879177172b8
Spencer Robinson <spencergrobinson@gmail.com>
2018-11-28 14:05:24 -0600

    removed nonexistant file reference in test.rb

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ require "./fsm.rb"
 require "./lexer.rb"
 
 fsm = FSM.new(nil)
-parser = Parser.new(fsm, ["parser","lexer.rb"])
+parser = Parser.new(fsm, ["lexer.rb"])
 while curr = parser.next
     p curr
 end
