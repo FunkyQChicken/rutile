37508758f41e5b06a8d504382d1fc3fd5adbae5b
Spencer Robinson <spencergrobinson@gmail.com>
2018-12-14 12:49:29 -0600

    removed print statements in lang.rb

---
+++ lang.rb
@@ -0,0 +1,38 @@
+require_relative "./nfa.rb"
+require_relative "./parser.rb"
+
+class Lang
+    def initialize
+        @tokens = {}
+        @nfas = []
+        @priority = 0
+        @nfa = nil
+    end
+
+    def tok(regex, symbol, &block)
+        new_nfa = NFA::to_nfa(regex, symbol)
+        @nfas << new_nfa
+        @tokens[symbol] = [@priority, block]
+        @priority += 1
+    end
+
+    def parse(files)
+        construct_nfa if @nfa.nil?
+        Parser.new(@nfa, files).parse.each do |token|
+            type  = get_type(token.type)
+            value = @tokens[type][1].call(token.string)
+        end
+    end
+
+    private
+    def get_type(types)
+        types.max {|val| @tokens[val].first} 
+    end
+
+    def construct_nfa
+        @nfa = @nfas.shift
+        @nfas.each {|n| @nfa.or n}
+        @nfas = nil
+    end
+
+end
