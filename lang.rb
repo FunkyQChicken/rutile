ce951059959323d4b0c0b8a073a6a5a37f88b9b6
Spencer Robinson <spencergrobinson@gmail.com>
2018-12-18 13:08:22 -0600

    allowed multiple tokes with the same token and ommiting token value to ignore:x

--- lang.rb
+++ lang.rb
@@ -3,32 +3,35 @@ require_relative "./lexer.rb"
 
 class Lang
     def initialize
-        @tokens = {}
+        @tokens = []
         @nfas = []
-        @priority = 0
+        @symbols = []
+        @id = 0
         @nfa = nil
     end
 
-    def tok(regex, symbol, &block)
-        new_nfa = NFA::to_nfa(regex, symbol)
+    def tok(regex, symbol = :ignore, &block)
+        new_nfa = NFA::to_nfa(regex, @id)
+        @symbols << symbol
         @nfas << new_nfa
-        @tokens[symbol] = [@priority, block]
-        @priority += 1
+        @tokens.append(block)
+        @id += 1
     end
 
     def parse(files)
         construct_nfa if @nfa.nil?
         Parser.new(@nfa, files).parse.each do |token|
-            type  = get_type(token.type)
-            value = @tokens[type][1].call(token.string)
+            id     = token.type.max
+            value  = @tokens[id].call(token.string)
+            symbol = @symbols[id]
+            if symbol != :ignore
+                #feed it to the grammar thing
+            end
         end
     end
 
     private
-    def get_type(types)
-        types.max {|val| @tokens[val].first} 
-    end
-
+    
     def construct_nfa
         @nfa = @nfas.shift
         @nfas.each {|n| @nfa.or n}
