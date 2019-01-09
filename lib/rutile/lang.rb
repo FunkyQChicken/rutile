b14dffb16234b5bd959a3113e493d4e0979f0663
Spencer Robinson <spencergrobinson@gmail.com>
2019-01-09 14:10:23 -0600

    added comments for public methods in lang.rb

--- lib/rutile/lang.rb
+++ lib/rutile/lang.rb
@@ -11,6 +11,11 @@ module Rutile
             @nfa = nil
         end
     
+        # add a new token to the language
+        #
+        # regex:  regular expression for the token
+        # symbol: name of the token when passed to parser
+        # block:  process the match into a value and return it
         def tok(regex, symbol = :ignore, &block)
             if block.nil?
                 block = Proc.new {|x|}
@@ -21,7 +26,10 @@ module Rutile
             @tokens.append(block)
             @id += 1
         end
-    
+        
+        # parse the given files
+        #
+        # files: list of file names and objects to be parsed in the given order
         def parse(files)
             construct_nfa if @nfa.nil?
             @parser = Parser.new(@nfa, files)
@@ -35,6 +43,12 @@ module Rutile
             end
         end
     
+        # add a file to be parsed by the language
+        # can be called mid-parse for things like imports
+        #
+        # file: file object or path to be parsed
+        # back: if true, add the file to the end of the list of files to be parsed
+        #       else, pause parsing of current file and start this one
         def add_file(file, back = false)
             if back
                 @parser.file_stack.unshift file
@@ -43,6 +57,13 @@ module Rutile
             end
         end
     
+        # generic run function for the language.
+        # parse files passed as arguments
+        # if none are passed, read from stdin.
+        #
+        # files: list of file objects or paths to be parsed
+        #        if it is "[]" then it will be set to stdin
+        #        before parsing.
         def run(files = ARGV)
             if files.nil? || files == []
                 files = [STDIN]
